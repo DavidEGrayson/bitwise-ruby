0936db9a1e619a2f605572de6d3b689f7ef07700
David Grayson <davidegrayson@gmail.com>
2018-03-14 00:46:59 -0700

    Day 2: Made a parser but accidentally made everything right associative.

--- day2/homework.rb
+++ day2/homework.rb
@@ -1,10 +1,3 @@
-# Assignment: Parse an infix expression compose of integer literls and the
-# following operators, highest to lowest precedence:
-#
-# unary -, unary ~
-# * / % << >> &
-# + - | ^
-
 class Lexer
   attr_reader :token
 
@@ -38,10 +31,70 @@ class Lexer
   end
 end
 
+# Assignment: Parse an infix expression composed of integer literals and the
+# following operators, highest to lowest precedence:
+#
+# unary -, unary ~ (right associative)
+# * / % << >> &    (left associative)
+# + - | ^          (left associative)
+#
+# EBNF grammar:
+#
+# number = "-" number | "~" number | integer
+# term = number { ( "*" | "/" | "%" | "<<" | ">>" | "&" ) term }
+# expr = term { ( "+" | "-" | "|" | "^" ) expr }
+class Parser
+  def initialize(str)
+    @lexer = Lexer.new(str)
+    @lexer.next_token
+  end
+
+  def parse_number
+    case @lexer.token
+    when :-, :~
+      op = @lexer.token
+      @lexer.next_token
+      [op, parse_number]
+    when Integer
+      num = @lexer.token
+      @lexer.next_token
+      num
+    else
+      raise "Expected a number or unary operator, got #{@lexer.token}."
+    end
+  end
+
+  def parse_term
+    number = parse_number
+    case @lexer.token
+    when :*, :/, :%, :<<, :>>, :&
+      op = @lexer.token
+      @lexer.next_token
+      [op, number, parse_term]
+    else
+      number
+    end
+  end
+
+  def parse_expression
+    term = parse_term
+    case @lexer.token
+    when nil
+      term
+    when :+, :-, :|, :^
+      op = @lexer.token
+      @lexer.next_token
+      [op, term, parse_expression]
+    else
+      raise "Expected expression operator, got #{@lexer.token.inspect}"
+    end
+  end
+end
+
 input = ARGF.read
-lexer = Lexer.new(input)
 
 # Test the lexer.
+lexer = Lexer.new(input)
 tokens = []
 while token = lexer.next_token
   tokens << token
@@ -49,3 +102,5 @@ end
 p tokens
 
 # Test the parser.
+parser = Parser.new(input)
+p parser.parse_expression
